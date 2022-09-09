12db1193d7c5f05250da262a863f2c22fe90e35a
Jirung-E <jh10590326@gmail.com>
2022-09-09 13:53:19 +0900

    Adder8bit

--- Chips/Adder8bit.py
+++ Chips/Adder8bit.py
@@ -1,11 +1,24 @@
 from .FullAdder import FullAdder
 
 def Adder8bit(bin1: str, bin2: str):
-    binary: str
+    r_bin1 = bin1[::-1]
+    r_bin2 = bin2[::-1]
+    binary = ""
+    carry_bit = 0
     for i in range(0, 8):
-        binary = binary + str(FullAdder(int(bin1[i]), int(bin2[i])))
-    return binary
+        res = FullAdder(int(r_bin1[i]), int(r_bin2[i]), carry_bit)
+        carry_bit = res[1]
+        binary = binary + str(res[0])
+    #result = "".join(reversed(binary))
+    result = binary[::-1]
+    return result
 
 def testAdder8bit():
-    print(Adder8bit("10101101", "00100001"))
+    print(f'10101101 00100001 | {Adder8bit("10101101", "00100001")}')
     assert Adder8bit("10101101", "00100001") == "11001110"
+    print(f'10101101 11111111 | {Adder8bit("10101101", "11111111")}')
+    assert Adder8bit("10101101", "11111111") == "10101100"
+    print(f'10101101 00000000 | {Adder8bit("10101101", "00000000")}')
+    assert Adder8bit("10101101", "00000000") == "10101101"
+    print(f'10101101 01111111 | {Adder8bit("10101101", "01111111")}')
+    assert Adder8bit("10101101", "01111111") == "00101100"
